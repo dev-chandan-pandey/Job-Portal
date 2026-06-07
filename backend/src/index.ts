5d8162f548e28f3e3f814026d90e285707d4751e
dev-chandan-pandey <iamchandanpandey11@gmail.com>
2026-06-07 18:40:46 +0530

    origin fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,6 +36,10 @@ export const instance = new Razorpay({
 const app = express();
 
 app.use(cors());
+app.use(cors({
+  origin: ["https://job-portal-tau-sepia.vercel.app"],
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
